4172fc7b0c22791c2e05453902d4a033974571fa
David Nalley <david@gnsa.us>
2011-12-08 16:30:41 -0500

    cleaning up kvmagent a bit more

--- manifests/kvmagent.pp
+++ manifests/kvmagent.pp
@@ -21,6 +21,17 @@ class cloudstack::kvmagent {
     require => Yumrepo[ 'cloudstack' ], 
   }
 
+  package { 'NetworkManager': 
+    ensure => absent;
+  }
+
+  service { 'network': 
+    ensure => running,
+    enabled => true, 
+    hasstatus => true,
+    requires => Package[ 'cloud-agent' ],
+  } 
+
   exec { '/usr/bin/cloud-setup-agent':
     creates  => '/var/log/cloud/setupAgent.log',
     require => [ 
@@ -40,17 +51,10 @@ class cloudstack::kvmagent {
 
 
  
-########## Also need to create a agent.properties stanza, and likely need to define
-########## IP address or name for management server - and do agent.properties as a template. 
-############ Need to do something that will take care of IP configuration
 ############ Need to do something that will take care of KVM - make sure module is loaded - need to define what tests cloud-setup-agent actually runs to test for KVM and ensure that we do those tests as well, and rectify if needed (do a reboot?? )
 ### Need to handle hostname addition as well - and probably a def gw and ensuring that DNS is set since
-### we are so backwards as to not use DHCP
 
 
-### IP Address thoughts:
-### Use a template based on /etc/sysconfig/ifcfg-ethX
-### By default only specify eth0, with liberal commenting about what to do in the event of needing to change our simple configuration (e.g. edit agent.properites, add additional network config, etc. 
 ### Require network to be enable
 ### Require NetworkManager be disabled (Is it installed by default, do we need to do a case?, perhaps we 'ensure absent') 
 ### Make sure we cycle network after deploying a ifcfg. 
